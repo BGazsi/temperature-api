1abd8ddb924ce599434b1dccbd95d6f88f0dad91
BGazsi <bgazsi94@gmail.com>
2021-01-22 23:33:36 +0100

    fix sorting

--- server/controllers/api-controller.js
+++ server/controllers/api-controller.js
@@ -3,7 +3,7 @@ const tempDb = require('../db/temperature');
 exports.getTemps = (req, res) => {
   tempDb.getAllTemps().then((rows) => {
     res.json({
-      temperatureData: rows.sort((row1, row2) => row1.timestamp - row2.timestamp),
+      temperatureData: rows.sort((row1, row2) => row2.timestamp - row1.timestamp),
     });
   }).catch((err) => {
     res.status(500);
