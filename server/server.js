41596130e1ff9522ade16665af97818db0fa092b
Bence-Gazsi <bence.gazsi@ibm.com>
2021-01-22 13:36:17 +0100

    cors whitelist

--- server/server.js
+++ server/server.js
@@ -3,6 +3,19 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const whiteList = ['https://bgazsi.github.io/', 'http://localhost:3001'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 const healthRoutes = require('./routes/health-route');
 const swaggerRoutes = require('./routes/swagger-route');
@@ -13,6 +26,7 @@ const app = express();
 // enable parsing of http request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 // routes and api calls
 app.use('/health', healthRoutes);
